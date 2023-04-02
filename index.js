84fd701dc5572fcfacfbeb54ab14b79d31579c04
Omar Mahmood <omarmahmood005@gmail.com>
2023-04-02 17:46:41 +0530

    renamed

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-const http = require('http');
+const http = require("http");
 const PORT = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!');
-});
+// const app = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello World!');
+// });
+const app = express();
 
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/`);
 });
